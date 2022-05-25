aa852446a79e1ed0a0783da6bc90fbff67b8e08b
Pavel Kus <pavel.kus.ext@deutsche-boerse.com>
2022-05-25 16:36:16 +0200

    separating wrapper function

--- greek.go
+++ greek.go
@@ -11,21 +11,39 @@ import "C"
 
 import "fmt"
 
+func CalcGreeks(strike, timeToMaturity, spot, riskFree, dividendYield, sigma float64,
+	value, delta, gamma, vega, theta *float64) {
+
+	var c_value, c_delta, c_gamma, c_vega, c_theta C.double
+
+	c_strike := C.double(strike)
+	c_timeToMaturity := C.double(timeToMaturity) //years
+	c_spot := C.double(spot)
+	c_riskFree := C.double(riskFree)
+	c_dividendYield := C.double(dividendYield)
+	c_sigma := C.double(sigma)
+
+	C.CalcGreeks(c_strike, c_timeToMaturity, c_spot, c_riskFree, c_dividendYield, c_sigma,
+		&c_value, &c_delta, &c_gamma, &c_vega, &c_theta)
+
+	*value = float64(c_value)
+	*delta = float64(c_delta)
+	*gamma = float64(c_gamma)
+	*vega = float64(c_vega)
+	*theta = float64(c_theta)
+}
+
 func main() {
-	strike := C.double(110.0)
-	timeToMaturity := C.double(0.5) //years
-	spot := C.double(100.0)
-	riskFree := C.double(.03)
-	dividendYield := C.double(0.0)
-	sigma := C.double(.20)
+	strike := 110.0
+	timeToMaturity := 0.5 //years
+	spot := 100.0
+	riskFree := .03
+	dividendYield := 0.0
+	sigma := .20
 
-	value := C.double(0.0)
-	delta := C.double(0.0)
-	gamma := C.double(0.0)
-	vega := C.double(0.0)
-	theta := C.double(0.0)
+	var value, delta, gamma, vega, theta float64
 
-	C.CalcGreeks(strike, timeToMaturity, spot, riskFree, dividendYield, sigma,
+	CalcGreeks(strike, timeToMaturity, spot, riskFree, dividendYield, sigma,
 		&value, &delta, &gamma, &vega, &theta)
 
 	fmt.Println("Value of ", strike, " call is ", value)
